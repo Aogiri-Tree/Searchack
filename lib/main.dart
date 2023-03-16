124852e7531661602628811284c5e4dd9b19a497
invokise <bilol586746@mail.ru>
2023-03-16 16:20:12 +0300

    Added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:searchack/pages/auth/auth.dart';
+import 'package:searchack/pages/main/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AuthPage(),
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
