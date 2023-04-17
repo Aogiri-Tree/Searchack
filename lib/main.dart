e21ec2baee1a6b255946dce1d9479bb5bb634724
invokise <bilol586746@mail.ru>
2023-04-17 05:01:23 +0300

    Connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:searchack/pages/auth/login/login_page.dart';
 import 'package:searchack/pages/main/main_page.dart';
+import 'package:searchack/services/firebase_auth_service.dart';
+import 'package:searchack/store/auth_store.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp(
+    firebaseAuthService: FirebaseAuthServiceImpl(FirebaseAuth.instance),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.firebaseAuthService});
+  final IFirebaseAuthService firebaseAuthService;
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +26,15 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color.fromRGBO(255, 255, 255, 1),
       ),
       debugShowCheckedModeBanner: false,
-      home: const MainPage(),
+      home: StreamBuilder<User?>(
+          stream: firebaseAuthService.authStateChanges,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const MainPage();
+            } else {
+              return LoginPage(authStore: AuthStore());
+            }
+          }),
     );
   }
 }
